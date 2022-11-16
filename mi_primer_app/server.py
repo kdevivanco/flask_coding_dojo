febe1749bd7f0f53e345c47d4564a80cae88ec6c
MacBookPro <macbookpro@MacBook-Pro-de-MacBookPro.local>
2022-11-15 19:06:31 -0500

    Subiendo primer assignment... rutas

---
+++ mi_primer_app/server.py
@@ -0,0 +1,26 @@
+from flask import Flask
+app = Flask (__name__)
+
+@app.route('/')
+def home():
+    return('<h3>Hola mundo</h3>')
+
+@app.route('/dojo')
+def ejemplo_1_dojo():
+    return '<h3>Dojo!</h3>'
+
+@app.route('/say/<nombre>')
+def ejemplo_2(nombre):
+    return f'<h3>{str(nombre)}</h3>'
+
+
+@app.route('/repeat/<some_int>/<word>')
+def ejemplo_3(some_int,word):
+    texto = [f'<h3>{str(word)}</h3>']*int(some_int)
+    texto = ' '.join(texto)
+    return texto
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
